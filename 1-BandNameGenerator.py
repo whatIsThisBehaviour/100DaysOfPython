d200e881bd1946dcbcb7aa88f628b0d66d2ae308
whatIsThisBehaviour <141537078+whatIsThisBehaviour@users.noreply.github.com>
2023-08-06 08:51:27 +0530

    Added files via upload.
    This is my first Python project where I learned how to take input from the user and display the output by combining multiple outputs and concatenating the string.

---
+++ 1-BandNameGenerator.py
@@ -0,0 +1,4 @@
+print("Welcome to the band generator !")
+name = input("What is your name ?\n")
+pet_name = input("What is the name of your pet ?\n")
+band_name = print("Your brand name can be " + name + " " + pet_name)
