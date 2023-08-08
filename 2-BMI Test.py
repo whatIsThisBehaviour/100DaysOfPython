202f236abb13232fed0772a1dc1e12f01162fe46
Jhelum <141537078+whatIsThisBehaviour@users.noreply.github.com>
2023-08-08 09:25:19 +0530

    Added files via upload.
    This is a BMI file made by using operators in Python.

---
+++ 2-BMI Test.py
@@ -0,0 +1,3 @@
+weight = float(input("Please enter your body weight : in kg - "))
+height = float(input("Please enter your height in meters : "))
+body_mass_index = print(round(weight/(height * height)))
