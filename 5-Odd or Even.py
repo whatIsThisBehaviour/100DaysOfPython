e6ef4e91fbf46b2e24f2edd4d6f1869aabef2326
Jhelum <141537078+whatIsThisBehaviour@users.noreply.github.com>
2023-08-10 19:10:55 +0530

    Add files via upload

---
+++ 5-Odd or Even.py
@@ -0,0 +1,5 @@
+number = int(input("Type the number you want to check for ODD/EVEN : "))
+if (number % 2 == 0):
+    print(f"{number} is an EVEN number")
+else:
+    print(f"{number} is an ODD number.")
